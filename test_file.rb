02f4417f5387e8a0f9769247da9dfbdfb310844c
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-02-16 15:12:30 -0600

    Added simple, working tests for Author and Location classes.

--- test_file.rb
+++ test_file.rb
@@ -25,46 +25,98 @@ class DatabaseTest < Minitest::Test
   def test_author_creation
     newauthor = Author.new(name: "Tester Testington")
     newauthor.insert("authors")
-    results = DATABASE.execute("SELECT name FROM authors WHERE id = #{newauthor.id}")
+    id = newauthor.id
+    results = DATABASE.execute("SELECT name FROM authors WHERE id = #{id}")
     
     added_record = results[0]
     
+    binding.pry
+    
     assert_equal(1, results.length)
-    assert_equal("Tester Testington", added_record[:name])
+    assert_equal("Tester Testington", added_record["name"])
   end
   
   def test_list_all_authors
     DATABASE.execute("DELETE FROM authors")
     a1 = Author.new(name: "Tester Testington")
     a2 = Author.new(name: "Test Faker")
-    a1.insert
-    a2.insert
+    a1.insert("authors")
+    a2.insert("authors")
     
-    assert_equal(2, Author.all.length)
+    return_table = Author.all("authors")
+    
+    assert_equal(2, return_table.length)
   end
   
   def test_delete_author
     DATABASE.execute("DELETE FROM authors")
     a1 = Author.new(name: "Tester Testington")
     a2 = Author.new(name: "Test Faker")
-    a1.insert
-    a2.insert
+    a1.insert("authors")
+    a2.insert("authors")
     
     Author.delete("authors", 2)
     
     results = DATABASE.execute("SELECT * FROM authors")
     remaining_record = results[0]
     
+    return_table = Author.all("authors")
+    
     #binding.pry #check return value of remaining_record
     
     assert_equal(1, remaining_record["id"])
-    assert_equal(1, Author.all.length)
+    assert_equal(1, return_table.length)
   end
 
 ##############################
 #       Location Tests       #
 ##############################
 
+  def test_add_location
+    newloc = Location.new(location_name: "Donut Stop")
+    newloc.insert("location_keys")
+    id = newloc.id
+    results = DATABASE.execute("SELECT location_name FROM location_keys WHERE id = #{id}")
+    
+    added_record = results[0]
+    
+    assert_equal(1, results.length)
+    assert_equal("Donut Stop", added_record["location_name"])
+  end
+  
+  def test_list_all_locations
+    DATABASE.execute("DELETE FROM location_keys")
+    l1 = Location.new(name: "Donut Stop")
+    l2 = Location.new(name: "Omaha Code School")
+    l1.insert("location_keys")
+    l2.insert("location_keys")
+    
+    return_table = Location.all("location_keys")
+    
+    assert_equal(2, return_table.length)
+  end
+  
+  def delete_location
+    DATABASE.execute("DELETE FROM location_keys")
+    l1 = Location.new(name: "Donut Stop")
+    l2 = Location.new(name: "Omaha Code School")
+    l1.insert("location_keys")
+    l2.insert("location_keys")
+        
+    Location.delete("location_keys", 2)
+    
+    results = DATABASE.execute("SELECT * FROM location_keys")
+    remaining_record = results[0]
+    
+    return_table = Location.all("location_keys")
+        
+    assert_equal(1, remaining_record["id"])
+    assert_equal(1, return_table.length)
+  end
+
+
 ##############################
 #       Article Tests        #
 ##############################
+
+end
